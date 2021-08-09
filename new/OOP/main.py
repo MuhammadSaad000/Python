949176cefe1bdf11cd8184bba22d5543078f1f6d
MuhammadSaad000 <67402416+MuhammadSaad000@users.noreply.github.com>
2021-08-09 15:41:37 +0500

    Add files via upload

---
+++ new/OOP/main.py
@@ -0,0 +1,14 @@
+from Calc import *   # importing local file / module
+
+
+obj1 = Calculator(2, 5)
+print(obj1.add())
+print(obj1.subtract())
+print(obj1.multiply())
+print(obj1.divide())
+print(obj1.mod())
+print(obj1.TakePower())
+print(obj1.takeSquareRoot())
+print(obj1.takeSin())
+print(obj1.takeCos())
+print(obj1.takeTan())
